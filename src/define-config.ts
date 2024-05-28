2b2695f6335a4a7f902c259de733c0a29dc5eac4
tianyingchun <tianyingchun@outlook.com>
2024-05-28 14:11:55 +0800

    ci: fix ci

--- src/define-config.ts
+++ src/define-config.ts
@@ -2,11 +2,12 @@ import { defineFlatConfig, FlatESLintConfig } from 'eslint-define-config';
 import { getDefaultIgnorePatterns } from './getDefaultIgnorePatterns.js';
 
 export const defineConfig = (
-  config: ReadonlyArray<FlatESLintConfig>
+  config: ReadonlyArray<FlatESLintConfig>,
+  ignores: string[] = []
 ): FlatESLintConfig[] => {
   return defineFlatConfig(
     config.concat({
-      ignores: getDefaultIgnorePatterns(),
+      ignores: getDefaultIgnorePatterns(ignores),
     })
   );
 };
--- src/getDefaultIgnorePatterns.ts
+++ src/getDefaultIgnorePatterns.ts
@@ -1,14 +1,14 @@
-export const getDefaultIgnorePatterns = () => {
+export const getDefaultIgnorePatterns = (ignores: string[] = []) => {
   return [
     // Hacky way to silence @yarnpkg/doctor about node_modules detection
     `**/${'node'}_modules`,
     '**/node_modules',
-    '.cache',
-    '.changeset',
-    '.github',
-    '.husky',
-    '.yarn',
-    '.next',
+    '**/.cache',
+    '**/.changeset',
+    '**/.github',
+    '**/.husky',
+    '**/.yarn',
+    '**/.next',
     '**/_release',
     '**/.cache',
     '**/build',
@@ -16,5 +16,6 @@ export const getDefaultIgnorePatterns = () => {
     '**/public',
     '**/.storybook',
     '**/storybook-static',
+    ...ignores,
   ];
 };
