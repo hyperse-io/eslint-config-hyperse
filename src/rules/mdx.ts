907c83cd47567f4a9c13c06d0a5a0403188b6a38
tianyingchun <tianyingchun@outlook.com>
2024-08-12 11:58:55 +0800

    chore: disable `lintCodeBlocks`

--- src/rules/mdx.ts
+++ src/rules/mdx.ts
@@ -4,12 +4,5 @@ import pluginMdx from 'eslint-plugin-mdx';
 export const mdx = defineFlatConfig([
   {
     ...(pluginMdx.flat as any),
-    // optional, if you want to lint code blocks at the same
-    processor: pluginMdx.createRemarkProcessor({
-      lintCodeBlocks: true,
-      // optional, if you want to disable language mapper, set it to `false`
-      // if you want to override the default language mapper inside, you can provide your own
-      languageMapper: {},
-    }),
   },
 ]);
