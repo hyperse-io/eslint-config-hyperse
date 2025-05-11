a873a324dae1cbba55beb9c98d59c98f0e83cd2c
tianyingchun <tianyingchun@outlook.com>
2025-05-11 16:46:37 +0800

    fix: remove `endOfLine` of prettier as default `LF`

--- src/getPrettierConfig.ts
+++ src/getPrettierConfig.ts
@@ -1,14 +1,13 @@
 import { type Config } from 'prettier';
 
 const prettierBaseConfig = {
-  // use single quotes instead of double quotes
-  singleQuote: true,
   // add semicolons at the end of statements
   semi: true,
+  // use single quotes instead of double quotes
+  singleQuote: true,
   // add trailing commas where valid in ES5 (objects, arrays, etc.)
   trailingComma: 'es5',
-  // maintain the line endings of the file
-  endOfLine: 'auto',
+  // keep it as default value for windows, mac endOfLine(lf).
 } satisfies Config;
 
 export const getPrettierConfig = (config: Config = {}): Config => {
